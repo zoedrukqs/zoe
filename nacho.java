f5f29542c1b304fc9272f6e75e2965a596e51347
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:22:26 -0500

    hola

---
+++ nacho.java
@@ -0,0 +1,18 @@
+package zoe;
+import java.util.Scanner;
+public class nacho {
+
+public static void main(String[] args) {
+    
+    int n1 = 0, n2 = 0;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("ingrese el numero 1");
+    n1 = sc.nextInt();
+    System.out.println("ingrese el numero 2");
+    n2 = sc.nextInt();
+
+    System.out.println("la suma de estos dos numeros es:");
+
+}
+
+}
---
+++ olaaa.java
@@ -0,0 +1,6 @@
+public class olaaa {
+public static void main(String[] args) {
+    
+    System.out.println("olaoaolaolaolaoal");
+}
+}
