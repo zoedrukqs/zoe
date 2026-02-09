240f1eec7d82538b99edb0d8e4a12350a0d032a8
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:29:30 -0500

    hola

--- nacho.java
+++ nacho.java
@@ -10,7 +10,7 @@ public static void main(String[] args) {
     n1 = sc.nextInt();
     System.out.println("ingrese el numero 2");
     n2 = sc.nextInt();
-
+    System.out.println(sumaNumeros n1, n2);
     System.out.println("la suma de estos dos numeros es:");
 
 }
